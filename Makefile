5b8095ba6636f3e2fa9e81d9a46475477ed50b7a
Abbath <pitongogi@gmail.com>
2018-11-10 22:48:50 +0100

    Update

---
+++ Makefile
@@ -0,0 +1,4 @@
+test:
+	cabal new-run Magic -v0 && cat Check/data.jsons | cabal new-run Checker -v0
+
+.PHONY: test
--- Types.hs
+++ Types.hs
@@ -6,11 +6,13 @@ module Types where
 
 import Data.Aeson
 import Data.Foldable
+import Debug.Trace
+import qualified Data.Text as T
 
 data Request = Task 
   { taskArguments :: [Value]
-  , taskExpected :: [Value]
-  } | Check { taskCheck :: Int }
+  , taskExpected :: Value
+  } | Check { taskCheck :: T.Text }
   deriving (Show, Eq)
 
 instance FromJSON Request where
@@ -22,7 +24,7 @@ instance ToJSON Request where
     toJSON (Check n) = object ["check" .= n] 
     toJSON (Task a e) = object ["arguments" .= a, "expected" .= e]
 
-data CaseRes = Ok Int | Failure [Value] | Err String | Dummy deriving (Show)
+data CaseRes = Ok T.Text | Failure [Value] | Err String | Dummy deriving (Show)
 
 foldCaseRess :: [CaseRes] -> CaseRes
 foldCaseRess = foldCaseRess' Dummy 
@@ -34,9 +36,9 @@ foldCaseRess = foldCaseRess' Dummy
      foldCaseRess' y [x@(Ok _)] = x
 
 instance ToJSON CaseRes where
-    toJSON (Ok n) = object ["status" .= ("ok" :: String), "result" .= n]
+    toJSON (Ok s) = object ["status" .= ("ok" :: String), "result" .= s]
     toJSON (Failure vs) = object ["status" .= ("failure" :: String), "result" .= vs]
-    toJSON (Err s) = object ["status" .= ("error" :: String), "result" .= ("unexpected" :: String)]
+    toJSON (Err s) = object ["status" .= ("error" :: String), "result" .= (s :: String)]
     toJSON Dummy = error "Should not be possible!"
 
 class Foo a where
@@ -50,5 +52,5 @@ fromResult (Success x) = x
 fromResult (Error s) = error s
 
 instance {-# OVERLAPPING #-} (FromJSON a, Foo b) => Foo (a -> b) where
-  run (s:ss) f = toJSON $ run ss (f (fromResult $ fromJSON s))
+  run a@(s:ss) f = toJSON $ run ss (f (fromResult $ fromJSON s))
   run _ _ = error "!!"
